8954c70a3a538fd384bc63ef1ac1157ae037d199
Mijon Utomi <Mijon.utomi@gmail.com>
2020-02-13 11:17:32 -0600

    need to not track csproj and sln

---
+++ BestBuyCrudExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestBuyCrudExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
